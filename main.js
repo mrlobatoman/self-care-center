9120ee88ed3c3935f072a1642a3ee36fbb2dffdc
Michael Lobato <mike.lobato.wft@gmail.com>
2022-12-18 10:59:37 -0800

    Adds: selectors to radio annd button

---
+++ main.js
@@ -0,0 +1,7 @@
+var affirmation = document.querySelector('#affirmation')
+var mantra = document.querySelector('#mantra')
+var receiveButton = document.querySelector('.receive-button')
+var messageBox = document.querySelector('.message-section')
+
+
+
